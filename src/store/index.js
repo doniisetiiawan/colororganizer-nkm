6632745566a86b9c3dc90c04d4f7b75a4275fdc9
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-08 16:44:08 +0700

    The Store

---
+++ src/store/index.js
@@ -0,0 +1,34 @@
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware,
+} from 'redux';
+import { colors, sort } from './reducers';
+import stateData from '../data/initialState.json';
+
+const logger = (store) => (next) => (action) => {
+  console.groupCollapsed('dispatching', action.type);
+  console.log('prev state', store.getState());
+  console.log('action', action);
+  const result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+const saver = (store) => (next) => (action) => {
+  const result = next(action);
+  localStorage['redux-store'] = JSON.stringify(
+    store.getState(),
+  );
+  return result;
+};
+
+const storeFactory = (initialState = stateData) => applyMiddleware(logger, saver)(createStore)(
+  combineReducers({ colors, sort }),
+  localStorage['redux-store']
+    ? JSON.parse(localStorage['redux-store'])
+    : initialState,
+);
+
+export default storeFactory;
